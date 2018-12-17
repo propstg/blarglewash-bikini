2e0aa84c0ab8f2e281947d027fa158e5ba522f95
gregory propst <gregory.propst@gmail.com>
2018-12-17 02:12:57 -0500

    add blips file

---
+++ client/blips.lua
@@ -0,0 +1,13 @@
+Blips = {}
+
+function Blips.InitBlips()
+    for i = 1, #Config.Locations do
+        local coords = Config.Locations[i]
+        local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+        SetBlipSprite(blip, 100)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString(_U('carwash_name'))
+        EndTextCommandSetBlipName(blip)
+    end
+end
