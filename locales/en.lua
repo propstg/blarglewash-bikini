082d30368cf290389148fcdce6b5f09a03067e30
Gregory Propst <g@prop.st>
2018-12-17 07:38:07 +0000

    adjust help text

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,6 @@
 Locales['en'] = {
     ['carwash_name']        = 'Car Wash',
-    ['hint_free']           = 'press ~INPUT_CONTEXT~ to wash your car',
-    ['hint_fee']            = 'press ~INPUT_CONTEXT~ to wash your car for ~r~$%s~s~.',
+    ['hint_free']           = 'press ~INPUT_VEH_HORN~ to wash your car',
+    ['hint_fee']            = 'press ~INPUT_VEH_HORN~ to wash your car for ~r~$%s~s~.',
     ['not_enough_money']    = 'you do not have enough money to wash your car.',
 }
