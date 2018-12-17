dee24ffd0199f158564c3a13bbc7d6c3e1cd83bc
gregory propst <gregory.propst@gmail.com>
2018-12-17 14:14:57 -0500

    adjust name

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Blarglewash Bikini Wash'
+description 'Blarglebottoms Bikini Wash'
 
 server_scripts {
     '@es_extended/locale.lua',
